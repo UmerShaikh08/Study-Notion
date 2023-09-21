a0f1d4ac6a010a7cde279cf0cc52a6221526cd8a
UmerShaikh08 <umershaikh10000@gmail.com>
2023-09-21 14:16:50 +0530

    .gitignore is now working

--- server/routes/contactUs.js
+++
@@ -1,8 +0,0 @@
-import { contactUs } from "../controller/contactUs.js";
-import { Router } from "express";
-
-const ContactUs = Router();
-
-ContactUs.post("/contact-us", contactUs);
-
-export { ContactUs };
